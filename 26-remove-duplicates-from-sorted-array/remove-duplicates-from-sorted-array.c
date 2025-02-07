905de299fef64acdf3dee98851a4ad497c40c14c
Snehil Singh <172139865+snehil-mod@users.noreply.github.com>
2025-02-07 13:16:05 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (27.68%) - LeetSync

---
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.c
@@ -0,0 +1,11 @@
+int removeDuplicates(int* nums, int numsSize) {
+    if(numsSize==0) return 0;
+    int j=0;
+    for(int i=1; i<numsSize; i++){
+        if(nums[i] != nums[j]){
+            j++;
+            nums[j] = nums[i];
+        }
+    }
+    return j+1;
+}
