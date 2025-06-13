a499602fc9989c4a5bdf9bcbfaec380dee2b1931
Snehil Singh <snehil.singh5151@gmail.com>
2025-06-14 01:21:50 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (51.87%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.c
@@ -0,0 +1,16 @@
+#include <stdbool.h>
+#include <stdlib.h>
+
+bool isPalindrome(char* s) {
+    int left = 0;
+    int right = strlen(s) - 1;
+    while(left < right){
+        while(left<right && !isalnum(s[left])) left++;
+        while(left<right && !isalnum(s[right])) right--;
+        
+        if(tolower(s[left]) != tolower(s[right])) return false;
+        left++;
+        right--;
+    }
+    return true;
+}
