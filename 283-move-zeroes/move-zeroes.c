e45a4abf3b1e23e93ddbca941caeb74f428baf54
Snehil Singh <snehil.singh5151@gmail.com>
2025-06-14 00:05:30 +0530

    Time: 91 ms (10.46%) | Memory: 19.6 MB (87.85%) - LeetSync

--- 283-move-zeroes/move-zeroes.c
+++ 283-move-zeroes/move-zeroes.c
@@ -1,4 +1,29 @@
 void moveZeroes(int* nums, int numsSize) {
+    int count = 0;
+    int j = 0;
+    for(int i=0; i<numsSize; i++){
+        if(nums[i] != 0){
+            nums[j] = nums[i];
+            j++;
+        }
+    }
+    for( j;j<numsSize; j++){
+        nums[j] = 0;
+    }
+    for(int i=0; i<numsSize;i++){
+        printf("%d ", nums[i]);
+    }
+}
+
+
+
+
+
+
+
+
+
+/*void moveZeroes(int* nums, int numsSize) {
     
     if(numsSize == 1){
         printf("%d", nums[0]);
@@ -14,4 +39,4 @@ void moveZeroes(int* nums, int numsSize) {
             nums[j++] = 0;
         }
     }
-}
+}*/
