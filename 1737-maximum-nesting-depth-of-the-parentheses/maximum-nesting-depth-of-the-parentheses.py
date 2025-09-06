5af7e42d4f0afba77f633ae06a19e8ffdeebab6a
Snehil Singh <snehil.singh5151@gmail.com>
2025-09-06 18:03:26 +0530

    Time: 3 ms (9.80%) | Memory: 17.7 MB (46.19%) - LeetSync

--- 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.py
+++ 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.py
@@ -1,14 +1,11 @@
 class Solution:
     def maxDepth(self, s: str) -> int:
-        stack = []
         maxm = 0
         counter = 0
         for i in s:
             if i == '(':
-                stack.append(i)
                 counter += 1
                 maxm = max(maxm, counter)
             elif i == ')':
-                stack.pop()
                 counter -= 1
         return maxm
