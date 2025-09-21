6253f7dc26f3f3e58bb975208c5119c36500890e
Snehil Singh <snehil.singh5151@gmail.com>
2025-09-22 00:01:09 +0530

    Time: 1214 ms (89.43%) | Memory: 17.9 MB (37.70%) - LeetSync

--- 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.py
+++ 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.py
@@ -2,9 +2,25 @@ class Solution:
     def beautySum(self, s: str) -> int:
         sum = 0
         n = len(s)
+        counter = {}
+        for i in range(n):
+            counter.clear()
+            for j in range(i, n):
+                counter[s[j]] = counter.get(s[j], 0) + 1
+                maxval = max(counter.values())
+                minval = min(counter.values())
+                sum += maxval - minval
+        return sum
+
+
+
+
+
+'''
         for i in range(n):
             counter = Counter()
             for j in range(i, n):
                 counter[s[j]] += 1
                 sum += max(counter.values()) - min(counter.values())
         return sum
+'''
